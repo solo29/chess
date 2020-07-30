23cfb88c4b3b82e1c3cabca45f00744e1d49d3b6
Solo <29plazma@gmail.com>
2020-07-30 19:20:38 +0400

    pages

---
+++ build/precache-manifest.b2a27f6f8e589c3d0bb5802959b70bb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3b49af312a607f48c8233ec60d5a87",
+    "url": "/chess/index.html"
+  },
+  {
+    "revision": "19102631f13025b9f74d",
+    "url": "/chess/static/css/main.fb44ab38.chunk.css"
+  },
+  {
+    "revision": "2d6aeacecc30ec89701e",
+    "url": "/chess/static/js/2.b2c8ad5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chess/static/js/2.b2c8ad5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19102631f13025b9f74d",
+    "url": "/chess/static/js/main.20400b2c.chunk.js"
+  },
+  {
+    "revision": "fe54e1bce9cc9d1a3729",
+    "url": "/chess/static/js/runtime-main.bff3f106.js"
+  },
+  {
+    "revision": "d39f1d02cda5131fc4c815f276a0521d",
+    "url": "/chess/static/media/knight.d39f1d02.svg"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7d35f6d9a54f043e785da191f6bc546a.js"
+  "/chess/precache-manifest.b2a27f6f8e589c3d0bb5802959b70bb1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/chess/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
